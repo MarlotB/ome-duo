01f790378778fb67f526a07470d3ddffbadfc63f
Marlot Bastiaenen <marlotbastiaenen@gmail.com>
2018-02-22 16:44:49 +0100

    Poging tot geld geluid toevoegen, nog niet gelukt.

--- Assets/_Scripts/Player_Score.cs
+++ Assets/_Scripts/Player_Score.cs
@@ -16,6 +16,13 @@ public class Player_Score : MonoBehaviour
     public float duration = 1.0F;
     public Renderer rend;
 
+	AudioSource m_MyAudioSource;
+
+    //Play the music
+    bool m_Play;
+    //Detect when you use the toggle, ensures music isn’t played multiple times
+    bool m_ToggleChange;
+
     void Start()
     {
 		darkBlue = new Color32(93, 114, 118, 0);
@@ -48,6 +55,26 @@ public class Player_Score : MonoBehaviour
 			trig.gameObject.SetActive (false);
 			playerMoney = playerMoney - 40;
 			ScoreText.text = "Geld: €" + playerMoney.ToString ();
+
+			//Fetch the AudioSource from the GameObject
+        	m_MyAudioSource = GetComponent<AudioSource>();
+
+			//Check to see if you just set the toggle to positive
+       		if (m_Play == true && m_ToggleChange == true)
+        	{
+            	//Play the audio you attach to the AudioSource component
+            	m_MyAudioSource.Play();
+            	//Ensure audio doesn’t play more than once
+            	m_ToggleChange = false;
+      	  	}
+        	//Check if you just set the toggle to false
+        	if (m_Play == false && m_ToggleChange == true)
+        	{
+            	//Stop the audio
+            	m_MyAudioSource.Stop();
+            	//Ensure audio doesn’t play more than once
+            	m_ToggleChange = false;
+        	}
 		}
 		if (trig.gameObject.CompareTag ("Food")) {
 			trig.gameObject.SetActive (false);
@@ -65,6 +92,19 @@ public class Player_Score : MonoBehaviour
 			mainCamera.backgroundColor = Color.Lerp(darkBlue, lightBlue, Mathf.PingPong(Time.time, 1));
 		}
 
+    }
+
+	void OnGUI()
+    {
+        //Switch this toggle to activate and deactivate the parent GameObject
+        m_Play = GUI.Toggle(new Rect(10, 10, 100, 30), m_Play, "Play Music");
+
+        //Detect if there is a change with the toggle
+        if (GUI.changed)
+        {
+            //Change to true to show that there was just a change in the toggle state
+            m_ToggleChange = true;
+        }
     }
     
 } 
